03d572f2d7fdd8555838889876b119197b774310
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-09-26 08:06:40 +0100

    creation de susteme de l'inscription de l'utilisateur

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const app = express();
 const { utilisateurs } = require("./models");
 const port = 8080;
 
-app.use(express.json());
+
 app.set('view engine', 'ejs');
-app.set('view', './views');
+app.set('views', './views');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+});
+
+app.get('/inscription', (req, res) => {
+    res.render('inscription', { title: 'Inscription - WealthWave' });
+});
 
 app.get("/users", async (req, res) => {
   const users = await utilisateurs.findAll();
@@ -13,6 +24,7 @@ app.get("/users", async (req, res) => {
 });
 
 app.post("/users", async (req, res) => {
+//   res.send(req.body);
   const { nom, prenom, email, password } = req.body;
   const newUser = await utilisateurs.create({ nom, prenom, email, password });
   res.json(newUser);
