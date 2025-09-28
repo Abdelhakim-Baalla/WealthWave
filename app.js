b7f8eba4251470ce84e2895737144af042885c55
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-09-28 23:09:54 +0100

    Creation de la page de creation de nouvelle mot de passe avec les afficher

--- app.js
+++ app.js
@@ -280,6 +280,10 @@ app.post("/motdepasseoublie", nonConnecte, async (req, res) => {
   }
 });
 
+app.get("/restorer-mot-de-passe", nonConnecte, (req, res) => {
+  res.render('restorationDeMotDePasse');
+});
+
 app.use((req, res, next) => {
   res.status(404).render("404");
 });
