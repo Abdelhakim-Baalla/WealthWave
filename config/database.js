f9cd8f7b526fc94ba179c2a83f1efe5ac9da506f
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-10-05 18:40:42 +0100

    Ajoutent des enviroenment

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
-
+require("dotenv").config();
 // console.log(Sequelize);
 
-const sequelize = new Sequelize("wealthwave", "root", "123456", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   logging: false,
 });
